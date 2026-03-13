cbda2cfcf84221c11a7d12d06be557e90dcdcf3a
RounakChoudhary <rounakchoudhary2005@gmail.com>
2026-03-14 02:08:47 +0530

    feat: implement url model

---
+++ src/models/url.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const UrlSchema = new mongoose.Schema({
+  shortCode: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  originalUrl: {
+    type: String,
+    required: true
+  },
+  clicks: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model("Url", UrlSchema)
